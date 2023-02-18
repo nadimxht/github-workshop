f6908741d215fc10857dc119200c9eb23ed37bcb
nadimxht <nadimxht@gmail.com>
2023-02-18 11:27:45 +0100

    Added new picture in app.js

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Button} from "react-bootstrap"
+
+// import imgSrc from './assets/logo-og.png'
+function App() {
+
+
+  return (
+    <div className="App">
+      <img src="./logo-og.png" />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Test.jsx
@@ -0,0 +1,12 @@
+
+function Profile(){
+    
+    return(
+        <>
+        hello
+        </>
+    )
+} 
+
+export default Profile ; 
+
